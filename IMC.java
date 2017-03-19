767a81f82dd281a8d0022f6c326a277da0995acc
gagliardibr <bru.gagliardi@hotmail.com>
2017-03-19 17:15:24 -0300

    Update IMC.java

--- IMC.java
+++ IMC.java
@@ -1,8 +1,8 @@
 public class IMC {
     public static void main(String[] args) {
       
-        double peso=51;
-        double altura=1.58;
+        double peso=60;
+        double altura=1.80;
         double IMC =(peso) / (altura*altura);
 
             System.out.println("Uma pessoa com peso "
