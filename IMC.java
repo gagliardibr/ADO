83062de556cdaaf8b5759e5d6e8fc3755872c760
gagliardibr <bru.gagliardi@hotmail.com>
2017-03-19 15:57:24 -0300

    Update IMC.java

---
+++ IMC.java
@@ -0,0 +1,17 @@
+public class IMC {
+    public static void main(String[] args) {
+      
+        double peso=51;
+        double altura=1.58;
+        double IMC =(peso) / (altura*altura);
+
+            System.out.println("Uma pessoa com peso "
+                    +  peso
+                    + " e altura "
+                    + altura
+                    + " tem IMC "
+                    + IMC);
+    }
+    
+    
+}
